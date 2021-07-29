9bfc3995c4908892fd490269bf0e081895c2b6d3
AlfilAlex <cabimax@outlook.com>
2021-07-28 21:35:29 -0500

    Feature aded: new responsive navbar

---
+++ navbar.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const icons = document.querySelectorAll('.icono');
+const navbar = document.querySelector('#hidden-menu');
+
+const menu_click = () => {
+    navbar.classList.toggle('hidden')
+}
+
+for (const icon of icons) {
+    icon.addEventListener('click', menu_click);
+}
+
+
+
+//
